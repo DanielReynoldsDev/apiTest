dfcf65688bea0a66d128cfae59552d441f256f07
DanielReynoldsDev <80770868+DanielReynoldsDev@users.noreply.github.com>
2022-07-15 11:55:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+
+
+class Program
+{
+    HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        Program program = new Program();
+        await program.GetJoke();
+    }
+
+    public async Task GetJoke()
+    {
+        string response = await client.GetStringAsync("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single");
+
+        Joke joke = JsonConvert.DeserializeObject<Joke>(response);
+        Console.WriteLine(joke.joke);
+
+    }
+
+
+}
+
+class Joke
+{
+    public string joke { get; set; }
+
+}
+
